5f85d494ccaeb7b2471f4cd82ff097eacbcac170
Sebastian Preston <38880204+sebprest@users.noreply.github.com>
2022-05-11 12:54:46 +0100

    Implement global style

---
+++ src/ts/globalStyle.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components'
+
+const globalStyle = createGlobalStyle`
+  body {
+    font-family: -apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif;
+  }
+`
+
+export default globalStyle
--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom'
 import { Normalize } from 'styled-normalize'
+import GlobalStyle from './globalStyle'
 
 import App from '^/app'
 
 ReactDOM.render(
   <>
+    <GlobalStyle />
     <Normalize />
     <App />
   </>,
