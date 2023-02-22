c897ea5d65ac1d52bd33115a1a6ebb2aac440d02
Sebastian Preston <38880204+sebprest@users.noreply.github.com>
2023-02-23 00:42:29 +0700

    Move API_ROOT to separate consts file

---
+++ src/ts/state/consts.ts
@@ -0,0 +1,1 @@
+export const API_ROOT = 'https://api.coingecko.com/api/v3'
--- src/ts/state/tokensList/index.ts
+++ src/ts/state/tokensList/index.ts
@@ -1,9 +1,8 @@
 import useSWR from 'swr'
 import fetcher from '^/state/genericFetcher'
 import { Token } from '^/state/types'
+import { API_ROOT } from '^/state/consts'
 import { TokensList } from './types'
-
-const API_ROOT = 'https://api.coingecko.com/api/v3'
 
 export const useTokensList = (): TokensList => {
   const { data, error } = useSWR([`${API_ROOT}/coins/markets`, { queryParams: { vs_currency: 'usd' } }], fetcher<Token[]>)
